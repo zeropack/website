ac002bb2fed2024e0f84b1bf5f9b90d86b8f11dc
zeropack <hello@zeropack.co>
2026-08-18 16:13:50 +0930

    Register ZP-AU-2026-001 AU article

--- src/content/articles/generated/au/index.ts
+++ src/content/articles/generated/au/index.ts
@@ -1,3 +1,4 @@
 import type { Article } from "../../types";
+import { compostablePackagingWaFogoGuidance } from "./compostable-packaging-wa-fogo-guidance";
 
-export const generatedAuArticles: Article[] = [];
+export const generatedAuArticles: Article[] = [compostablePackagingWaFogoGuidance];

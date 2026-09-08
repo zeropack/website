e70777cbc9f29d1599e8c5552f4ea4440c5dd5a8
zeropack <hello@zeropack.co>
2026-09-08 19:32:38 +0930

    Add UK canonical sitemap endpoint

---
+++ src/app/market-seo/uk/sitemap/route.ts
@@ -0,0 +1,18 @@
+import { buildMarketUrl } from "@/lib/marketRouting";
+
+export const dynamic = "force-static";
+
+function xmlEscape(value: string): string {
+  return value.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\"/g, "&quot;");
+}
+
+export function GET() {
+  const urls = [buildMarketUrl("uk", "/"), buildMarketUrl("uk", "/custom-compostable-mailers")];
+  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls
+    .map((url) => `  <url><loc>${xmlEscape(url)}</loc></url>`)
+    .join("\n")}\n</urlset>`;
+
+  return new Response(body, {
+    headers: { "Content-Type": "application/xml; charset=utf-8", "Cache-Control": "public, max-age=3600" },
+  });
+}

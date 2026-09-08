faaaf7da92cc70bd9afa613cec67da384fa0d1ea
zeropack <hello@zeropack.co>
2026-09-08 20:01:45 +0930

    Use canonical global origin in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
-import { getSiteUrl } from "@/lib/site";
+import { MARKET_ROUTES } from "@/lib/marketRouting";
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${getSiteUrl()}/sitemap.xml`,
+    sitemap: `${MARKET_ROUTES.global.canonicalOrigin}/sitemap.xml`,
   };
 }

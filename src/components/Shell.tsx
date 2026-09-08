b8dc45f63844a06acb915c2b6e7737a9083481f3
zeropack <hello@zeropack.co>
2026-09-08 20:14:02 +0930

    Wire regional suggestion into site shell

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -4,12 +4,14 @@ import { Footer } from "./Footer";
 import { StickyMobileCTA } from "./StickyMobileCTA";
 import { StickyDesktopCTA } from "./StickyDesktopCTA";
 import { MarketingClient } from "./MarketingClient";
+import { MarketExperience } from "./MarketExperience";
 
 export function Shell({ children }: { children: ReactNode }) {
   return (
     <>
       <MarketingClient />
       <Header />
+      <MarketExperience />
       <main className="flex-1 pb-24 pt-[var(--site-header-height)] sm:pb-6">{children}</main>
       <Footer />
       <StickyMobileCTA />

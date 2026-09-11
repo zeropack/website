b3b709ed81058f7b14c5b0217845b121f64106b7
zeropack <hello@zeropack.co>
2026-09-11 10:19:36 +0930

    Make legacy funnel redirect permanent

--- src/app/trend-packaging-funnel/page.tsx
+++ src/app/trend-packaging-funnel/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default function TrendPackagingFunnelPage() {
-  redirect("/custom-compostable-mailers/");
+  permanentRedirect("/custom-compostable-mailers/");
 }

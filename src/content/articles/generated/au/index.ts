f0e0f8643db2101d0aa015c14799eb454cf52876
zeropack <hello@zeropack.co>
2026-09-17 20:18:47 +0930

    Move AS 5810 article to Australia

--- src/content/articles/generated/au/index.ts
+++ src/content/articles/generated/au/index.ts
@@ -1,4 +1,8 @@
 import type { Article } from "../../types";
 import { compostablePackagingWaFogoGuidance } from "./compostable-packaging-wa-fogo-guidance";
+import { whatIsAs5810HomeCompostableCertification } from "./what-is-as5810-home-compostable-certification";
 
-export const generatedAuArticles: Article[] = [compostablePackagingWaFogoGuidance];
+export const generatedAuArticles: Article[] = [
+  compostablePackagingWaFogoGuidance,
+  whatIsAs5810HomeCompostableCertification,
+];
